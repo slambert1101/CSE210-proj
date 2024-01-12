ad351e00decb3e3bc76f74a4a5f8cba78f7667be
Samuel Lambert <lam20008@byui.edu>
2024-01-12 12:47:15 -0700

    completed assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "an F.";
+
+        if (grade >= 90)
+        {
+            letter = "an A.";
+        }
+        else if (grade >= 80)
+        {
+            letter = "a B.";
+        }
+        else if (grade >= 70)
+        {
+            letter = "a C.";
+        }
+        else if (grade >= 60)
+        {
+            letter = "a D.";
+        }
+        else
+        {
+            letter = "an F.";
+        }
+
+        Console.WriteLine($"You received {letter}");
+
+        if (grade >=70)
+        {
+            Console.WriteLine("Congradulations on passing!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
+       
     }
 }
