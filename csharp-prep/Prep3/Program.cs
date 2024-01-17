68fdb40231104c19464a319fe7e3f55c9413653b
Samuel Lambert <lam20008@byui.edu>
2024-01-17 11:19:24 -0700

    finishedThePrep3Project

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        
+        Console.WriteLine("Guess the magic number between 1 and 100!");
+    
+
+        int guess = magicNumber + 1;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string response = Console.ReadLine();
+            guess = int.Parse(response);
+
+
+            if(guess > magicNumber)
+            {
+                Console.WriteLine("lower");
+            }
+
+            if(guess < magicNumber)
+            {
+                Console.WriteLine("higher");
+            }
+
+            if(guess == magicNumber)
+            {
+                Console.WriteLine("That is correct.");
+            }
+
+
+        } while (guess != magicNumber);
+
+
     }
 }
