955db170e4026f5f3357d73a7a0d7dd9336bbb97
Samuel Lambert <lam20008@byui.edu>
2024-01-17 17:27:44 -0700

    Completed prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int SquareNumber(int x)
+        {
+            int numberSquared = x * x;
+            return numberSquared;
+        }
+
+        static void DisplayResult(string userName, int square)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {square}");
+
+        }
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int numberSquared = SquareNumber(favNumber);
+        DisplayResult(name, numberSquared);
     }
 }
