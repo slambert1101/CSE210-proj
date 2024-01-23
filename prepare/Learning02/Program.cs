36df807046c1c2a1b82b5870f971f2a6f6abdaad
Samuel Lambert <lam20008@byui.edu>
2024-01-22 17:30:21 -0700

    Finished assignment.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "KFC";
+        job1._jobTitle = "Manager";
+        job1._startYear = 2017;
+        job1._endYear = 2020;
+        
+
+        Job job2 = new Job();
+        job2._company = "DARPA";
+        job2._jobTitle = "Cheif engineer";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+        
+        Resume r = new Resume();
+        r._name = "Samuel Lambert";
+
+        r._jobs.Add(job1);
+        r._jobs.Add(job2);
+        r.Display();
     }
 }
