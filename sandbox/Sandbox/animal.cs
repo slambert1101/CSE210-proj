7a1eb6c276e7d1ab9315a4f6c7222bfa95986e39
Samuel Lambert <lam20008@byui.edu>
2024-02-05 12:20:55 -0700

    Changed public to protected for name and sound

--- sandbox/Sandbox/animal.cs
+++ sandbox/Sandbox/animal.cs
@@ -1,8 +1,8 @@
 public abstract class Animal
 {
 
-    public string name;
-    public string sound;
+    protected string name;
+    protected string sound;
 
     //protected means only child class can access constructor vvv
     protected Animal(string name, string sound)
