b94756086b1b31be853658ee6f9cf262e530fb10
Samuel Lambert <lam20008@byui.edu>
2024-02-21 16:02:39 -0700

    completed project

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignment = new MathAssignment("Richard L", "Advanced Math", "section 1.1", "problems 8-12");
+
+        // Console.WriteLine(assignment.GetSummary());
+        // Console.WriteLine(assignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mauricio Sanchez", "American History", "The effects of the Civil War");
+
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string name, string topic, string title) : base(name, topic)
+    {
+        _title = title;
+    }
+
+
+   public string GetWritingInformation()
+   {
+        string name = base.GetStudentName();
+        return $"{_title} by {name}";
+   }
+
+
+}
